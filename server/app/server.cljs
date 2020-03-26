d12a9ef6e9b41ff0245ee7e75739fc83132fe572
jiyinyiyong <jiyinyiyong@gmail.com>
2020-03-27 00:27:41 +0800

    try to deploy new package in new format; alpha release

--- server/app/server.cljs
+++ server/app/server.cljs
@@ -5,28 +5,27 @@
                    [cljs.core.async :refer [<! >! timeout chan]]
                    [shallow-diff.patch :refer [patch]]
                    [cirru-sepal.analyze :refer [write-file ns->path]]
+                   [cirru-edn.core :as cirru-edn]
                    [fipp.edn :refer [pprint]]
-                   [app.walk :refer [walk]])
+                   [app.walk :refer [walk]]
+                   ["fs" :as fs]
+                   ["http" :as http]
+                   ["path" :as path]
+                   ["mkdirp" :as mkdirp])
   (:require-macros [cljs.core.async.macros :refer [go]]))
-
-(def fs (js/require "fs"))
-(def http (js/require "http"))
-(def path (js/require "path"))
-(def mkdirp (js/require "mkdirp"))
 
 (def ir-path
   (or
     (get (js->clj js/process.argv) 2)
-    (if (fs.existsSync "ir.edn") "ir.edn")
-    (if (fs.existsSync "stack-sepal.ir") "stack-sepal.ir")
+    (if (fs/existsSync "stack.cirru") "stack.cirru")
     (do
-      (println "Missing file: ir.edn or stack-sepal.ir not found!")
+      (println "Missing file: stack.cirru not found!")
       (.exit js/process 1))))
 
 (def ref-sepal
   (atom
-    (if (fs.existsSync ir-path)
-        (read-string (fs.readFileSync ir-path "utf8"))
+    (if (fs/existsSync ir-path)
+        (cirru-edn/parse (fs/readFileSync ir-path "utf8"))
         (do (.log js/console (str "Error: " ir-path " does not exist!"))
             (.exit js/process 1)))))
 
@@ -34,7 +33,7 @@
 (def out-folder (or js/process.env.out (get-in @ref-sepal [:options :src]) "src/"))
 (def port (js/parseInt (or js/process.env.port (get-in @ref-sepal [:options :port]) "7010")))
 
-(defn read-body [req]
+(defn read-body [^js req]
   (let [body-ref (atom "")
         body-chan (chan)]
     (.on req "data" (fn [chunk] (swap! body-ref str chunk)))
@@ -42,19 +41,19 @@
     body-chan))
 
 (defn rewrite-file! [content]
-  (fs.writeFileSync ir-path (with-out-str (pprint content {:width 120}))))
+  (fs/writeFileSync ir-path (cirru-edn/write content)))
 
 (defn write-by-file [pkg ns-part file-info]
   (let [file-name (str (ns->path pkg ns-part) extension)
         content (write-file file-info)
-        file-target (path.join out-folder file-name)
-        container (path.dirname file-target)]
+        file-target (path/join out-folder file-name)
+        container (path/dirname file-target)]
     (println "File compiled:" file-name)
-    (if (not (fs.existsSync container))
+    (if (not (fs/existsSync container))
         (do
           (println "Creating folder:" container)
-          (mkdirp.sync container)))
-    (fs.writeFileSync file-target content)))
+          (mkdirp/sync container)))
+    (fs/writeFileSync file-target content)))
 
 (defn compare-write-source! [sepal-data]
   (doseq [entry (:files sepal-data)]
@@ -68,7 +67,7 @@
     (let [[ns-part file-info] entry]
       (write-by-file (:package sepal-data) ns-part file-info))))
 
-(defn req-handler [req res]
+(defn req-handler [^js req ^js res]
   (if (some? req.headers.origin)
     (.setHeader res "Access-Control-Allow-Origin" req.headers.origin))
   (.setHeader res "Content-Type" "text/edn; charset=UTF-8")
@@ -92,7 +91,7 @@
     (.end res (str "Unknown:" req.method))))
 
 (defn create-app! []
-  (let [app (http.createServer req-handler)]
+  (let [app (http/createServer req-handler)]
     (.listen app port)
     (println "File:" ir-path)
     (println "Port:" port)
@@ -110,7 +109,7 @@
   (map
     (fn [ns-part]
       (string/replace
-        (path.join out-folder
+        (path/join out-folder
                    (string/replace (:package @ref-sepal) "." "/")
                    (str (string/replace ns-part "." "/") extension))
         "-" "_"))
@@ -128,12 +127,12 @@
       ; (println "alive-files" alive-files)
       ; (println "existing-files" existing-files)
       (doseq [file-path removed-files]
-        (fs.unlinkSync file-path)
+        (fs/unlinkSync file-path)
         (println "Redundant file:" file-path)))))
 
 (.on js/process "SIGINT"
   (fn []
-    (if (fs.existsSync out-folder) (check-removed!))
+    (if (fs/existsSync out-folder) (check-removed!))
     (.exit js/process)))
 
 (defn reload! []
